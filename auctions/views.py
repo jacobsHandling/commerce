e8b1456a9ea103ecfb026f456523954db91f3db4
Jacob Shandling <jacobshandling@icloud.com>
2021-05-14 14:48:59 -0700

    Require login to add new listing; update default login path

--- auctions/views.py
+++ auctions/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -64,7 +65,7 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
-
+@login_required
 def new_listing(request):
     """Handles creating a new listing"""
     if request.method == 'POST':
