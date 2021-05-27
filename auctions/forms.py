a6794b69516219a80faa0de679a0ceedb6521e35
Jacob Shandling <jacobshandling@icloud.com>
2021-05-26 18:04:21 -0700

    style ListingForm, for new listings

--- auctions/forms.py
+++ auctions/forms.py
@@ -14,9 +14,42 @@ from .models import Listing, Bid, ListingComment
     # place the form’s data in its cleaned_data attribute.
 
 class ListingForm(ModelForm):
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['categories'].widget.attrs.update({'name': 'Categories (hold "command" on mac to select multiple)'})
+
     class Meta:
         model = Listing
-        widgets = {'categories': forms.CheckboxSelectMultiple}
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                    }
+                ),
+            'categories': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control'
+                    }
+                ),
+            'starting_price': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                    }
+                ),
+            'image_url': forms.URLInput(
+                attrs={
+                    'class': 'form-control'
+                    }
+                ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control'
+                    }
+                ),
+            }
+
+
         fields = ['title', 'categories','starting_price', 'image_url', 'description']
 
 class PartialBidForm(ModelForm):
